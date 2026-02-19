f8750f626b93a3f2339c2e52f9e1923d482fb304
Sggz221 <sggz221@gmail.com>
2026-02-19 02:56:32 +0100

    Logotipo 1

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy, ChangeDetectorRef } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -8,6 +8,32 @@ import { RouterLink } from '@angular/router';
   templateUrl: './home.html',
   styleUrls: ['./home.css']
 })
-export class Home {
+export class Home implements OnInit, OnDestroy {
+  activeImageIndex: number = 0;
+  private intervalId: any;
 
+  constructor(private cdr: ChangeDetectorRef) { }
+
+  ngOnInit() {
+    this.startAnimationLoop();
+  }
+
+  ngOnDestroy() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
+
+  private startAnimationLoop() {
+    this.intervalId = setInterval(() => {
+      if (this.activeImageIndex === 0) {
+        // Switch to a random image (1-4)
+        this.activeImageIndex = Math.floor(Math.random() * 4) + 1;
+      } else {
+        // Switch back to base image (0)
+        this.activeImageIndex = 0;
+      }
+      this.cdr.detectChanges(); // Manually trigger change detection just in case
+    }, 2200); // Change every 2.2 seconds
+  }
 }
