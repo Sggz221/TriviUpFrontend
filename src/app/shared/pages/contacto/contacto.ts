7dfc33b1ee815c961df5f1ba7bb7c4a135c2c7b7
Samuel Gómez <sggz221@gmail.com>
2026-05-24 19:00:35 +0200

    Fix commit anterior

--- src/app/shared/pages/contacto/contacto.ts
+++ src/app/shared/pages/contacto/contacto.ts
@@ -2,7 +2,7 @@ import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { NavbarComponent } from '../navbar/navbar.component';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
 
 @Component({
     selector: 'app-contacto',
